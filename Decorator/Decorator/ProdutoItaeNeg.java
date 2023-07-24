2b740c4bb4094db7dc4c88e9ba7a8c47ccb5dad4
gab-lacerda <103219296+gab-lacerda@users.noreply.github.com>
2023-07-24 20:00:19 -0300

    Add files via upload

---
+++ Decorator/Decorator/ProdutoItaeNeg.java
@@ -0,0 +1,11 @@
+public class ProdutoItaeNeg extends ProdutoDecorator{
+
+	public ProdutoItaeNeg(Produto p) {
+		super(p);
+	}
+	
+	@Override
+	public String formataParaImpressao() {
+		return "font-weight:bold; font-style: italic\">" +  super.formataParaImpressao();
+	}
+}
---
+++ Decorator/Decorator/ProdutoNegrito.java
@@ -0,0 +1,13 @@
+
+public class ProdutoNegrito extends ProdutoDecorator {
+
+	public ProdutoNegrito(Produto p) {
+		super(p);
+	}
+	
+	@Override
+	public String formataParaImpressao() {
+		return "font-weight: bold;\">" + super.formataParaImpressao();
+	}
+	
+}
