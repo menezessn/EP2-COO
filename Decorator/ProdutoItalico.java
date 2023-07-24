9a47095f7774c593e6518915f35d2336558a15dd
gab-lacerda <103219296+gab-lacerda@users.noreply.github.com>
2023-07-24 20:01:07 -0300

    Add files via upload

---
+++ Decorator/ProdutoItalico.java
@@ -0,0 +1,11 @@
+
+public class ProdutoItalico extends ProdutoDecorator {
+
+	public ProdutoItalico(Produto p) {
+		super(p);
+	}
+
+	public String formataParaImpressao() {
+		return "font-style: italic;\">" + super.formataParaImpressao();
+	}
+}
