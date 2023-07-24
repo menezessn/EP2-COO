1d0ea899211db0cd087cbe9a950179073187142d
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-07-24 12:34:04 -0300

    refactor

--- CriterioDescricao.java
+++
@@ -1,6 +0,0 @@
-public class CriterioDescricao implements CriterioOrdenacao{
-    @Override
-    public int comparar(Produto p1, Produto p2) {
-        return p1.getDescricao().compareToIgnoreCase(p2.getDescricao());
-    }
-}
--- CriterioOrdenacao.java
+++
@@ -1,4 +0,0 @@
-//Interface para implementar as classes com os criterios de ordenação a serem utilizados para o padrão stratefy
-public interface CriterioOrdenacao {
-    int comparar(Produto p1, Produto p2);
-}
